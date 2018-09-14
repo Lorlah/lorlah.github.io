23b2372f22fd2fb465122662fccdc04ec187e444
Lorlah <omololasalehu@gmail.com>
2018-09-14 03:31:56 +0100

    Etablished major breakpoints

--- Portfolio-Site/js/app.js
+++ Portfolio-Site/js/app.js
@@ -1,13 +1,13 @@
 // JavaScript Document
 
-let menuObject = document.querySelector(".top-header");
+let menuObject = document.querySelector(".menu-icon");
 let siteContent = document.querySelector(".main-content");
 let siteNavigation= document.querySelector(".site-navigation");
 
 menuObject.addEventListener("click", function(event){
     siteNavigation.classList.toggle("open");
 });
-
+ 
 siteContent.addEventListener("click", function(event){
     siteNavigation.classList.remove("open");
 })
