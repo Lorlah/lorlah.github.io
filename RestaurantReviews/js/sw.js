782dc220375c7f63c16704c557c818679b3bc457
Lorlah <omololasalehu@gmail.com>
2018-11-20 23:31:21 +0100

    Fix: Modified sw.js

--- RestaurantReviews/js/sw.js
+++ RestaurantReviews/js/sw.js
@@ -6,24 +6,7 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(cacheName).then(function(cache) {
             return cache.addAll([
-                './',
-                './index.html',
-                './restaurant.html',
-                './js/main.js',
-                './js/dbhelper.js',
-                './js/restaurant_info.js',
-                './css/styles.css',
-                './data/restaurants.json',
-                './img/1.jpg',
-                './img/2.jpg',
-                './img/3.jpg',
-                './img/4.jpg',
-                './img/5.jpg',
-                './img/6.jpg',
-                './img/7.jpg',
-                './img/8.jpg',
-                './img/9.jpg',
-                './img/10.jpg'
+                './'
                 ]);
         })
     );
